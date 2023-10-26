bca572bb54c1f7d0b242f84983deb3ef1cb0c31a
Sunghyun Ko <sunko@cx2r8s4.42seoul.kr>
2023-10-26 23:37:46 +0900

    parsing

--- minishell.c
+++ minishell.c
@@ -6,11 +6,12 @@
 /*   By: sunko <sunko@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 22:09:25 by sunko             #+#    #+#             */
-/*   Updated: 2023/10/25 22:58:56 by sunko            ###   ########.fr       */
+/*   Updated: 2023/10/26 23:37:28 by sunko            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "token/token.h"
 
 int main(int argc , char *argv[])
 {
@@ -46,8 +47,137 @@ int main(int argc , char *argv[])
 	exit(EXIT_SUCCESS);
 }
 
-int	parse_execute(struct t_source *src)
+int	parse_execute(t_source *src)
 {
+	t_token_list	*token_list;
 
+	skip_white_space(src);
+	token_list = tokenizer(src);
+
+}
+
+t_token_list	*tokenizer(t_source *src)
+{
+	t_token_list	*token_list;
+	t_token			*new_token;
+	char			cur;
+	char			next;
+	int				idx;
+
+	token_list = create_token_list();
+	while (peek_char(src) != EOF)
+	{
+		cur = next_char(src);
+		new_token = create_token();
+		if (cur == '&')
+		{
+			next = peek_char(src);
+			if (next != '&')
+				continue ; // error?
+			new_token->string = "&&";
+			new_token->type = LOGICAL_END;
+			next_char(src);
+		}
+		else if (cur == '|')
+		{
+			next = peek_char(src);
+			if (next == '|')
+			{
+				new_token->string = "||";
+				new_token->type = LOGICAL_OR;
+				next_char(src);
+			}
+			else
+			{
+				new_token->string == "|";
+				new_token->type = PIPE;
+			}
+		}
+		else if (cur == '\'' || cur == '\"')
+		{
+			int i = 0;
+			idx = find_close_quote(src->buffer + src->curpos);
+			if (!idx)
+				continue; // error handler
+			new_token->string = (char *)malloc(sizeof(idx + 1));
+			if (!new_token->string)
+				continue; // error handler
+			new_token->string[0] = cur;
+			if (cur == '\'')
+				new_token->type = SIGLE_QUOTE;
+			else if (cur == '\"')
+				new_token->type = DOUBLE_QUOTE;
+			while (idx--)
+				new_token->string[++i] = next_char(src);
+		}
+		else if (cur == '*')
+		{
+			new_token->string = "*";
+			new_token->type = WILDCARD;
+		}
+		else if (cur == '<')
+		{
+			next = peek_char(src);
+			if (next == '<')
+			{
+				new_token->string = "<<";
+				new_token->type = RIGHT_APPEND;
+				next_char(src);
+			}
+			else
+			{
+				new_token->string = "<";
+				new_token->type = RIGHT_REDIR;
+			}
+		}
+		else if (cur == '>')
+		{
+			next = peek_char(src);
+			if (next == '<')
+			{
+				new_token->string = "<<";
+				new_token->type = LEFT_APPEND;
+				next_char(src);
+			}
+			else
+			{
+				new_token->string = "<";
+				new_token->type = LEFT_REDIR;
+			}
+		}
+
+
+	}
+}
+
+size_t	find_close_quote(char *str)
+{
+	char	quote;
+	size_t	idx;
+	size_t	len;
+
+	quote = str[0];
+	if (quote != '\'' && quote != '\"')
+		return	(0);
+	idx = 0;
+	len = ft_strlen(str);
+	while (++idx < len)
+	{
+		if (str[idx] == quote)
+			return (idx);
+	}
+	return 0;
+}
+
+
+
+t_token	*create_token(void)
+{
+	t_token		*new_token;
+
+	new_token = (t_token *)malloc(sizeof(t_token));
+	if (!new_token)
+		return (NULL);
+	return new_token;
 }
 
--- token/token.h
+++ token/token.h
@@ -6,7 +6,7 @@
 /*   By: sunko <sunko@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 23:34:19 by sunko             #+#    #+#             */
-/*   Updated: 2023/08/05 12:14:52 by sunko            ###   ########.fr       */
+/*   Updated: 2023/10/26 23:22:44 by sunko            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,34 @@
 # include "../minishell.h"
 
 # define TOKEN_NUM			18
-# define LOGICAL_END		0
-# define LOGICAL_OR			1
-# define SIGLE_QUOTE		3
-# define DOUBLE_QUOTE		4
-# define PIPE				5
-# define LEFR_PAREN			6
-# define RIGHT_PAREN		7
-# define RIGHT_REDIR		13
-# define LEFT_REDIR			14
-# define RIGHT_APPEND		15
-# define LEFT_APPEND		16
+// # define LOGICAL_END		0
+// # define LOGICAL_OR			1
+// # define SIGLE_QUOTE		3
+// # define DOUBLE_QUOTE		4
+// # define PIPE				5
+// # define LEFR_PAREN			6
+// # define RIGHT_PAREN		7
+// # define RIGHT_REDIR		13
+// # define LEFT_REDIR			14
+// # define RIGHT_APPEND		15
+// # define LEFT_APPEND		16
 
 typedef enum e_token_list
 {
-	logical_end,
-	logical_or,
-	wildcard,
-	s_quote,
-	d_quote,
-	t_pipe,
-	left_paren,
-	right_paren,
-	left_brace,
-	right_brace,
-	left_braket,
-	right_braket,
-	semi_colon,
-	right_redir,
-	left_redir,
-	right_append,
-	left_append,
-	dollar_sign
+	LOGICAL_END,
+	LOGICAL_OR,
+	WILDCARD,
+	SIGLE_QUOTE,
+	DOUBLE_QUOTE,
+	PIPE,
+	LEFR_PAREN,
+	RIGHT_PAREN,
+	RIGHT_REDIR,
+	LEFT_REDIR,
+	RIGHT_APPEND,
+	LEFT_APPEND,
+	DOLLAR_SIGN,
+	WORD
 }	t_token_type;
 
 typedef struct s_token
@@ -70,23 +66,6 @@ typedef struct s_token_list
 	int		num_of_data;
 }	t_token_list;
 
-/* quote_scan.c */
-int				get_quote_len(char *string, char quote);
-char			*quote_malloc(int len);
-void			sigle_quote_scan(t_token_list *list, char *command, size_t *i);
-void			double_quote_scan(t_token_list *list, char *command, size_t *i);
-
-/* scan.c */
-t_token			scan(char *command);
-
-/* token_list.c */
-t_node			*create_node(void);
-t_token_list	*create_token_list(void);
-t_token			*create_token_sample(void);
-void			push_back(t_token_list *token_list, t_node *node);
-
-/* init_token.c */
-void			init_token(t_token *token_list);
 
 #endif
 
